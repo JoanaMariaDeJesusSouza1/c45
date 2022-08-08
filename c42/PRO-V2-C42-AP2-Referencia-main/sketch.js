2d001fa8619c4a9ebf65767ac70c4bb90df095a4
joana maria de jesus souza <98822804+JoanaMariaDeJesusSouza1@users.noreply.github.com>
2022-08-08 19:31:13 -0300

    Add files via upload

---
+++ c42/PRO-V2-C42-AP2-Referencia-main/sketch.js
@@ -0,0 +1,19 @@
+
+
+function preload() {
+
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+
+}
+
+function draw() {
+  background(backgroundImage);
+ 
+  drawSprites()
+}
+
+
